c2f955214b83d66f3f6d5b9ad3572030fc3d0809
nuttycom <nuttycom@13f79535-47bb-0310-9956-ffa450edef68>
2006-10-26 21:45:56 +0000

    Minor documentation and license changes
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/sandbox/pipeline/trunk@468151 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/commons/pipeline/StageDriverFactory.java
+++ src/main/java/org/apache/commons/pipeline/StageDriverFactory.java
@@ -1,27 +1,33 @@
 /*
- * Copyright 2005 The Apache Software Foundation
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
  *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.    
+ */ 
 
 package org.apache.commons.pipeline;
 
 /**
- * This interface represents a factory that is used by a {@link Pipeline} to create
+ * <p>This interface represents a factory that is used by a {@link Pipeline} to create
  * a driver for a {@link Stage} when that stage is added to the pipeline. The factory
  * pattern is used here to ensure that each stage is run by a unique driver
- * instance.
+ * instance.</p>
  *
+ * <p>In order to guarantee that StageDriverFactory instances can be used
+ * effectively in configuration frameworks, each StageDriverFactory implementation
+ * <em>must</em> provide a no-argument constructor.
  */
 public interface StageDriverFactory {
     /**
